718fd09c8afe128f3345ce47de8c34b446ceaf03
Kanhapong <112235436+Kanhapong@users.noreply.github.com>
2022-10-08 11:35:09 +0700

    Add files via upload

---
+++ src/pages/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <navbar />
+  <section class="home">
+
+    <div class="image">
+        <img src="images/Lx.png" alt="">
+    </div>
+
+    <div class="content">
+        <h3>hi, i am {{name}}</h3>
+        <span>{{motto}}</span>
+        <p>{{content}}</p>
+        <a href="about.html" class="btn"> about me <i class="fas fa-user"></i> </a>
+    </div>
+
+</section>
+</template>
+
+<script>
+import Navbar from '../components/Navbar.vue'
+export default {
+  components: { Navbar },
+      data() {
+        return {
+            name: "Kanhapong Kanhakul",
+            motto: "Yo!",
+            content: "A gift And a curse is feel everything so deeply",
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
