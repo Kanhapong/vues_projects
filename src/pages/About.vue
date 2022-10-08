740395aaa5a2b3c29d575145a41df247203b4aa6
Kanhapong <112235436+Kanhapong@users.noreply.github.com>
2022-10-08 12:07:00 +0700

    Add files via upload

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,170 +1,150 @@
 <template>
     <div>
-  <navbar />
-  <section class="about">
+        <navbar />
+        <section class="about">
 
-        <h1 class="heading"> about <span>me</span> </h1>
+            <h1 class="heading"> about <span>me</span> </h1>
 
-        <div class="row">
+            <div class="row">
 
-            <div class="info-container">
+                <div class="info-container">
 
-                <h1>personal info</h1>
+                    <h1>personal info</h1>
 
-                <div class="box-container">
+                    <div class="box-container">
+
+                        <div class="box">
+                            <h3> <span>name : </span>{{name}}</h3>
+                            <h3> <span>age : </span>{{age}}</h3>
+                            <h3> <span>email : </span>{{email}}</h3>
+                            <h3> <span>address : </span>{{address}}</h3>
+                        </div>
+
+                        <div class="box">
+                            <h3> <span>freelance : </span>{{freelance}}</h3>
+                            <h3> <span>skill : </span>{{skill}}</h3>
+                            <h3> <span>experience : </span>{{experience}}</h3>
+                            <h3> <span>language : </span>{{language}}</h3>
+                        </div>
+
+                    </div>
+
+                    <a href="/profilewithcss/files/resume.pdf" class="btn"> download CV <i class="fas fa-download"></i>
+                    </a>
+
+                </div>
+
+                <div class="count-container">
 
                     <div class="box">
-                        <h3> <span>name : </span>{{name}}</h3>
-                        <h3> <span>age : </span>{{age}}</h3>
-                        <h3> <span>email : </span>{{email}}</h3>
-                        <h3> <span>address : </span>{{address}}</h3>
+                        <h3>5+</h3>
+                        <p>years of experience</p>
                     </div>
 
                     <div class="box">
-                        <h3> <span>freelance : </span>{{freelance}}</h3>
-                        <h3> <span>skill : </span>{{skill}}</h3>
-                        <h3> <span>experience : </span>{{experience}}</h3>
-                        <h3> <span>language : </span>{{language}}</h3>
+                        <h3>10+</h3>
+                        <p>happy clients</p>
+                    </div>
+
+                    <div class="box">
+                        <h3>20+</h3>
+                        <p>project completed</p>
+                    </div>
+
+                    <div class="box">
+                        <h3>5+</h3>
+                        <p>Certificates</p>
                     </div>
 
                 </div>
 
-                <a href="/profilewithcss/files/resume.pdf" class="btn"> download CV <i class="fas fa-download"></i> </a>
-
             </div>
 
-            <div class="count-container">
+        </section>
 
-                <div class="box">
-                    <h3>5+</h3>
-                    <p>years of experience</p>
-                </div>
+        <!-- about section ends -->
 
-                <div class="box">
-                    <h3>10+</h3>
-                    <p>happy clients</p>
-                </div>
+        <!-- skills section starts  -->
 
-                <div class="box">
-                    <h3>20+</h3>
-                    <p>project completed</p>
-                </div>
+        <section class="skills">
 
-                <div class="box">
-                    <h3>5+</h3>
-                    <p>Certificates</p>
+            <h1 class="heading"> <span>my</span> skills </h1>
+
+            <div class="box-container">
+                <div class="box" v-for="s in skills" :key="s.name">
+                    <img :src="s.image">
+                    <h3>{{s.name}}</h3>
                 </div>
 
             </div>
 
-        </div>
+        </section>
 
-    </section>
+        <!-- skills section ends -->
 
-    <!-- about section ends -->
+        <!-- education section starts  -->
 
-    <!-- skills section starts  -->
+        <section class="education">
 
-    <section class="skills">
+            <h1 class="heading"> <span>my</span> experience </h1>
 
-        <h1 class="heading"> <span>my</span> skills </h1>
+            <div class="box-container">
 
-        <div class="box-container">
-            <div class="box">
-                <img src="images/icon-1.png">
-                <h3>html</h3>
-            </div>
-            <div class="box">
-                <img src="images/icon-2.png">
-                <h3>css</h3>
-            </div>
-            <div class="box">
-                <img src="images/icon-3.png">
-                <h3>javascript</h3>
-            </div>
-            <div class="box">
-                <img src="images/icon-4.png">
-                <h3>sass</h3>
-            </div>
-            <div class="box">
-                <img src="images/icon-5.png">
-                <h3>angular</h3>
-            </div>
-            <div class="box">
-                <img src="images/icon-6.png">
-                <h3>react.js</h3>
-            </div>
-        </div>
+                <div class="box" v-for="e in experiences" :key="e.years">
+                    <div class="box">
+                        <i class="fas fa-briefcase"></i>
+                        <span>{{e.years}}</span>
+                        <h3>{{e.company}}</h3>
+                        <p>{{e.jobs}}</p>
+                    </div>
+                </div>
 
-    </section>
-
-    <!-- skills section ends -->
-
-    <!-- education section starts  -->
-
-    <section class="education">
-
-        <h1 class="heading"> <span>my</span> experience </h1>
-
-        <div class="box-container">
-
-            <div class="box" v-for="e in experiences" :key="e.years">
-                <div class="box">
-                <i class="fas fa-briefcase"></i>
-                <span>{{e.years}}</span>
-                <h3>{{e.company}}</h3>
-                <p>{{e.jobs}}</p>
-            </div>
             </div>
 
-        </div>
-
-    </section>
+        </section>
     </div>
 </template>
 
 <script>
 import Navbar from '../components/Navbar.vue'
+import axios from "axios"
 export default {
-  components: { Navbar },
+    components: { Navbar },
+    methods: {
+        async fetchExperince() {
+            try {
+                let res = await axios.get("https://633fc566e44b83bc73c0f545.mockapi.io/profile/v1/experiences")
+                this.experiences = res.data
+            } catch (error) {
+            }
+        },
+        async fetchSkill() {
+            try {
+                let res = await axios.get("https://633fc566e44b83bc73c0f545.mockapi.io/profile/v1/skills")
+                this.skills = res.data
+            } catch (error) {
+
+            }
+        }
+
+    },
     data() {
         return {
             name: "Kanhapong Kanhakul",
             age: "19",
-            email:"kanhapong01@gmail.com",
+            email: "kanhapong01@gmail.com",
             address: "Nontaburi, Thailand 11110",
-            freelance:"available",
+            freelance: "available",
             skill: "Video Game.",
-            experience:"4 years",
-            language:"thai",
-            experiences: [{
-                years: "2017 - 2018",
-                company: "INTER EXPRESS LOGISTICS PRIVATE LIMITED",
-                jobs: "Assist the system analyst in conduct analysis, architecture, design database model for the new application.",
-            }, {
-                years: "2017 - 2018",
-                company: "PTT Digital Solutions Company Limited",
-                jobs: "Front-end coding to develop web applications follows by mock-up screen design. And back-end coding and test to configuration workflows and business processes.",
-            }, {
-                years: "2018 - 2018",
-                company: "Thai Beverage PLC",
-                jobs: "Develop new features to facilitate related procedures and tools if necessary, Troubleshoot, test, and maintain the core product software and databases to ensure strong optimization and functionality.",
-            }, {
-                years: "2019 - 2020",
-                company: "Cigna",
-                jobs: "Assist the system analyst in conduct analysis, architecture, design database model for the new application.",
-            }, {
-                years: "2019 - 2020",
-                company: "EMETWORKS",
-                jobs: "Develop new features to facilitate related procedures and tools if necessary.",
-            }, {
-                years: "2020 - 2021",
-                company: "uTech Solutions",
-                jobs: "Assist the system analyst in conduct analysis, architecture, design database model for the new application.",
-            }],
-
-
+            experience: "4 years",
+            language: "thai",
+            experiences: [],
+            skills: [],
         }
+    },
+    beforeMount() {
+        this.fetchExperince()
+        this.fetchSkill()
     }
 
 }
